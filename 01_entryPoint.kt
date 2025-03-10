d7dde04ea39e1396e5ddc7e745d5e43f6ef36bc3
Omkar Bhagat <onkarabhagat@outlook.com>
2025-03-10 10:24:26 +0530

    Initial Commit

---
+++ 01_entryPoint.kt
@@ -0,0 +1,11 @@
+/*
+    entry point function
+    The name of this function must always
+    be the same: `main`. If you name it `Main`
+    or `MAIN` or something else, it'll not gonna
+    work.
+*/
+
+fun main() {
+    println("Hello, World!")
+}
